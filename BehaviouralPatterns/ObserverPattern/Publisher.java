c1a12bf3e25b9c93dc2f8b245cb9cea782a4aaac
rishirajyadav <19ucs045@lnmiit.in>
2026-01-28 19:26:58 +0530

    commiting publisher code

---
+++ BehaviouralPatterns/ObserverPattern/Publisher.java
@@ -0,0 +1,8 @@
+
+
+public interface Publisher{
+
+    registerObserver();
+    removerObserver();
+    notifyObservers();
+}
---
+++ BehaviouralPatterns/ObserverPattern/PublisherData.java
@@ -0,0 +1,23 @@
+
+
+public class PublisherData implements Publisher {
+
+
+    @Override
+    public void registerObserver(){
+
+    }
+
+    @Override
+    removerObserver(){
+
+    }
+    @Override
+    notifyObservers(){
+
+    }
+
+    public static void main(String[] args){
+        System.out.println("Main class");
+    }
+}
